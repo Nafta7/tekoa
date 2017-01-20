0147c21282f715a3daf7eadc7a2f02c49de7d2f8
Nefla <neflasan@gmail.com>
2017-01-19 23:49:39 -0200

    add boot file

---
+++ app/index.js
@@ -0,0 +1,5 @@
+import Inferno from 'inferno'
+import Component from 'inferno-component'
+import AppContainer from './containers/AppContainer'
+
+Inferno.render(<AppContainer />, document.getElementById('app'))
