1a9bc0c8a14fa7f952f6d2d6c60d04dee3a6e021
Nefla <neflasan@gmail.com>
2017-01-19 23:48:18 -0200

    add constants

---
+++ app/constants/AppConstants.js
@@ -0,0 +1,6 @@
+const AppConstants = {
+  DEV_MODE: "dev",
+  PROD_MODE: "prod"
+}
+
+export default AppConstants
---
+++ app/constants/ContentType.js
@@ -0,0 +1,6 @@
+const ContentType = {
+  PLAYLIST: 'playlist',
+  CHANNEL: 'channel'
+}
+
+export default ContentType
