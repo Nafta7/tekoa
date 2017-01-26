7ef5762dd9c900d7e9d02f2357e11683fe88226e
Nefla <neflasan@gmail.com>
2017-01-26 17:54:03 -0200

    romove unused import

--- app/index.js
+++ app/index.js
@@ -1,5 +1,4 @@
 import Inferno from 'inferno'
-import Component from 'inferno-component'
 import AppContainer from './containers/AppContainer'
 
 Inferno.render(<AppContainer />, document.getElementById('app'))
