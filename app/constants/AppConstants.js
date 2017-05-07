a07684551bb1ef8ccad262cc0ebce4df6e2760cf
Nefla <neflasan@gmail.com>
2017-05-07 17:37:34 -0300

    update constants

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -2,8 +2,8 @@ const AppConstants = {
   DEV_MODE: "dev",
   PROD_MODE: "prod",
   THROTTLE_TIME_MS: 1000,
-  CONTENT_MAX_RESULTS: 10,
-  PLAYLIST_ITEMS_MAX_RESULTS: 25
+  CONTENT_MAX_RESULTS: 25,
+  PLAYLIST_ITEMS_MAX_RESULTS: 50
 }
 
 export default AppConstants
